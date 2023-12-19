e2c88e24896320ef803770f13433ce1cbb145180
max <maxpete121@gmail.com>
2023-12-19 15:38:36 -0700

    Job functions

--- src/controllers/JobsController.js
+++ src/controllers/JobsController.js
@@ -9,14 +9,26 @@ export class JobsController extends BaseController{
         super('api/jobs')
         this.router
         .get('', this.getJobs)
+        .get('/find/:jobId', this.findJob)
         .post('', this.postJob)
+        .delete('/:jobId', this.deleteJob)
     }
 
     async postJob(request, response, next){
         try {
             const jobData = request.body
-            const job = jobService.postJob(jobData)
+            const job = await jobService.postJob(jobData)
             response.send(job)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async findJob(request, response, next){
+        try {
+            const jobId = request.params.jobId
+            const found = await jobService.findJob(jobId)
+            response.send(found)
         } catch (error) {
             next(error)
         }
@@ -27,6 +39,16 @@ export class JobsController extends BaseController{
         try {
             const jobs = await jobService.getJobs()
             response.send(jobs)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async deleteJob(request, response, next){
+        try {
+            const jobId = request.params.jobId
+            const deleteJob = await jobService.deleteJob(jobId)
+            response.send(deleteJob)
         } catch (error) {
             next(error)
         }
--- src/services/JobService.js
+++ src/services/JobService.js
@@ -11,9 +11,23 @@ class JobService{
         return newJob
     }
 
+    async findJob(jobId){
+        const foundJob = await dbContext.Jobs.findById(jobId)
+        if(foundJob == undefined){
+            throw new Error('Cant find job at that Id')
+        }
+        return foundJob
+    }
+
     async getJobs(){
         const allJob = await dbContext.Jobs.find()
         return allJob
+    }
+
+    async deleteJob(jobId){
+        const foundJob = await dbContext.Jobs.findById(jobId)
+        await foundJob.remove()
+        return`Job listing removed`
     }
 }
 
